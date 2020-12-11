63eb2f5f362f19d6fd8ea5189ea280dcc966cf94
Jérémy Chaut <JCHAUT@3KLES.local>
2020-12-11 16:00:14 +0100

    Add tableConfig model

---
+++ projects/kles-material-table/src/lib/models/tableconfig.model.ts
@@ -0,0 +1,14 @@
+import { Type } from '@angular/core';
+import { Sort } from '@angular/material/sort';
+import { KlesColumnConfig } from './columnconfig.model';
+import { Options } from './options.model';
+import { AbstractKlesTableService } from '../services/abstracttable.service';
+
+export interface KlesTableConfig {
+    tableComponent: Type<any>;
+    columns: KlesColumnConfig[];
+    tableService: AbstractKlesTableService;
+    selectionMode?: boolean;
+    options?: Options<any>;
+    sortConfig?: Sort;
+}
