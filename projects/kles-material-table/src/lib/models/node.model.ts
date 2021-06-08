abca5b3fe617771c0734d0bc39f0f51efd83acd8
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2021-06-08 15:26:38 +0200

    Clear node model

--- projects/kles-material-table/src/lib/models/node.model.ts
+++ projects/kles-material-table/src/lib/models/node.model.ts
@@ -4,19 +4,3 @@ export interface Node {
     options?: { [key: string]: string }
     children?: Node[];
 }
-
-export interface SearchableNode extends Node {
-    id: string;
-    children?: SearchableNode[];
-}
-
-export interface TreeTableNode extends SearchableNode {
-    depth: number;
-    isVisible: boolean;
-    isExpanded: boolean;
-    children?: TreeTableNode[];
-}
-
-export interface NodeInTree extends SearchableNode {
-    pathToRoot: SearchableNode[];
-}
