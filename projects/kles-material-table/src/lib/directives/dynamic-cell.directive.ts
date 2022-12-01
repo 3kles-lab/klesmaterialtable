8283cd46a3aa40a2f13b359c02140aada69e0e61
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2022-12-01 10:28:30 +0100

    Fix deprecated

--- projects/kles-material-table/src/lib/directives/dynamic-cell.directive.ts
+++ projects/kles-material-table/src/lib/directives/dynamic-cell.directive.ts
@@ -1,9 +1,9 @@
-import { KlesDynamicFieldDirective } from "@3kles/kles-material-dynamicforms";
-import { Directive, OnInit, OnChanges, OnDestroy, Input, SimpleChanges } from "@angular/core";
-import { UntypedFormGroup } from "@angular/forms";
-import { KlesLeafComponent } from "../component/treetable/cell/leaf.component";
-import { KlesNodeComponent } from "../component/treetable/cell/node.component";
-import { KlesTreeColumnConfig } from "../models/columnconfig.model";
+import { KlesDynamicFieldDirective } from '@3kles/kles-material-dynamicforms';
+import { Directive, OnInit, OnChanges, OnDestroy, Input, SimpleChanges } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { KlesLeafComponent } from '../component/treetable/cell/leaf.component';
+import { KlesNodeComponent } from '../component/treetable/cell/node.component';
+import { KlesTreeColumnConfig } from '../models/columnconfig.model';
 
 
 @Directive({
@@ -28,19 +28,12 @@ export class KlesDynamicCellDirective extends KlesDynamicFieldDirective implemen
     }
 
     buildComponent() {
-        const factory = this.resolver.resolveComponentFactory(
-            // ~~this.row.value._status.childrenCounter > 0 && this.column.canExpand ? KlesNodeComponent : KlesLeafComponent
-            this.column.canExpand ? KlesNodeComponent : KlesLeafComponent
-        );
+        if (this.componentRef) { this.componentRef.destroy(); }
 
-        if (this.componentRef) this.componentRef.destroy();
-
-        this.componentRef = this.container.createComponent(factory);
+        this.componentRef = this.container.createComponent(this.column.canExpand ? KlesNodeComponent : KlesLeafComponent);
         this.componentRef.instance.field = this.field;
         this.componentRef.instance.group = this.group;
         this.componentRef.instance.row = this.row;
         this.componentRef.instance.column = this.column;
-
     }
-
 }
