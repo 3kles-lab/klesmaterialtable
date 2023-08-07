6d1c2f045ecd1cc94a59ca8fe5b41dc99918b934
Charles Da Silva Costa <CDASILVA@3KLES.local>
2023-08-07 14:24:52 +0200

    Clear logs

--- projects/kles-material-table/src/lib/services/lazy/lazytreetable.service.ts
+++ projects/kles-material-table/src/lib/services/lazy/lazytreetable.service.ts
@@ -42,8 +42,6 @@ export class KlesLazyTreetableService extends classes(DefaultKlesTreetableServic
     }
 
     protected changeChildrenVisibility(node: UntypedFormGroup, visibility: boolean) {
-        // console.log('changeChildrenVisibility=', node);
-        // node.controls._status.patchValue({ ...node.getRawValue()._status, children: node.getRawValue().tempChildren }, { emitEvent: false, onlySelf: true });
         node.value._status.children?.forEach(child => {
             const childGroup = this.table.getFormArray().controls.find(control => control.value._id === child._id) as UntypedFormGroup;
             if (childGroup) {
@@ -72,32 +70,19 @@ export class KlesLazyTreetableService extends classes(DefaultKlesTreetableServic
     }
 
     addChild(parentId: string, record): UntypedFormGroup {
-        console.log('##AddChild ', record);
-        console.log('##Searchable=', this.table.searchableTree);
-        // const parent = this.table.searchableTree.find(s => s._id === parentId);
-        // console.log('parent', parent);
         const treeTableTree = this.table.searchableTree.map(st => this.table.converterService.toTreeTableTree(st));
-        console.log('TreeTable Complete=', treeTableTree);
         const parent = treeTableTree.find(s => s._id === parentId);
-        console.log('parent', parent);
-        const parentDepth = ~~parent?.depth;// this.table.treeService.getNodeDepth(this.table.searchableTree, parent);
-        console.log('parent depth=', parentDepth);
+        const parentDepth = ~~parent?.depth;
 
         if (parent) {
             const searchableNode = this.table.converterService.toSearchableTree(record);
-            console.log(searchableNode)
             const treeNode = this.table.converterService.toTreeTableTree(searchableNode);
             treeNode.depth = ~~parentDepth + 1;
-            console.log('treeNode', treeNode);
             const groups = this.table.createFormNode(treeNode);
-            console.log('Groups=', groups);
-            console.log('List FormArray=', this.table.getFormArray().controls);
-            console.log('Find Index Parent=', this.table.getFormArray().controls.findIndex((group: UntypedFormGroup) => group.value._id === parentId));
             const indexParent = this.table.getFormArray().controls.findIndex((group: UntypedFormGroup) => group.value._id === parentId);
             const index = indexParent;
             + (parent.children?.length || 0);
 
-            console.log('index', index);
             if (parent.children) {
                 parent.children.push({ value: groups[0].getRawValue() });
             } else {
@@ -107,20 +92,8 @@ export class KlesLazyTreetableService extends classes(DefaultKlesTreetableServic
             this.table._lines[indexParent].children = parent.children;
             this.table._lines.splice(index + 1, 0, record);
             groups.forEach((group, i) => {
-                console.log('Insert Group=', group);
                 this.table.getFormArray().insert(index + (i + 1), group);
             });
-            console.log('new _lines=', this.table._lines);
-            // this.table.searchableTree=this.table.getFormArray().controls
-            console.log('new lines control=', (this.table.getFormArray() as UntypedFormArray).controls.map(line => {
-                return {
-                    value: line.value,
-                    _id: line.value?._id,
-                    children: (line.value?._id === parentId) ? parent.children : line.value?._status?.children,
-                    childrenCounter: (line.value?._id === parentId) ? ~~parent.children?.length : ~~line.value?._status?.childrenCounter,
-                    isBusy: (line.value?._id === parentId) ? false : line.value?._status?.isBusy || false
-                }
-            }))
             this.table.searchableTree = (this.table.getFormArray() as UntypedFormArray).controls.map(line => {
                 return {
                     value: (line.value?._id === parentId) ? { ...line.value, children: parent.children, childrenCounter: ~~parent.children?.length } : line.value,
@@ -128,14 +101,11 @@ export class KlesLazyTreetableService extends classes(DefaultKlesTreetableServic
                     children: (line.value?._id === parentId) ? parent.children : line.value?._status?.children,
                     childrenCounter: (line.value?._id === parentId) ? ~~parent.children?.length : ~~line.value?._status?.childrenCounter,
                     isBusy: line.value?._status?.isBusy || false
-                    // isBusy: (line.value?._id === parentId) ? false : line.value?._status?.isBusy || false
+                    
                 }
             });
-            // this.table.searchableTree = this.table._lines.map(t => this.table.converterService.toSearchableTree(t));
 
             this.updateDataSource();
-
-            // return groups[0];
         }
 
         return null;
@@ -150,16 +120,11 @@ export class KlesLazyTreetableService extends classes(DefaultKlesTreetableServic
     }
 
     deleteChildren(parentId: string) {
-        console.log('##Delete Children ', parentId);
-        console.log('##Searchable=', this.table.searchableTree);
         const treeTableTree = this.table.searchableTree.map(st => this.table.converterService.toTreeTableTree(st));
-        console.log('TreeTable Complete=', treeTableTree);
         const parent = treeTableTree.find(s => s._id === parentId);
-        console.log('parent', _.cloneDeep(parent));
 
         if (parent && parent.children) {
             const listChildrenId = parent.children.map(m => m.value?._id);
-            console.log('Children _id to delete=', listChildrenId);
             parent.children = null;
             const indexParent = this.table.getFormArray().controls.findIndex((group: UntypedFormGroup) => group.value._id === parentId);
             this.table._lines[indexParent] = parent;
