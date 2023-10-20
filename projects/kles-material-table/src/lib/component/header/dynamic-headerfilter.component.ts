e28fff446d03254c68f2b3902bd22527e764915c
Quentin Tellier <QTELLIER@3KLES.local>
2023-10-20 10:03:25 +0200

    Fix KlesFormDynamicHeaderFilterComponent

--- projects/kles-material-table/src/lib/component/header/dynamic-headerfilter.component.ts
+++ projects/kles-material-table/src/lib/component/header/dynamic-headerfilter.component.ts
@@ -7,7 +7,7 @@ import { IKlesHeaderFieldConfig } from '../../models/header-field.config.model';
     template: `
     <div mat-sort-header [disabled]="!field.sortable"><span>{{ field.label | translate}}</span></div>
     <div (click)="stopPropagation($event)" *ngIf="field.filterComponent" style="display: inline-flex;">
-            <ng-container klesComponentHeader [component]="field.filterComponent" [group]="group" [field]="field" >
+            <ng-container klesComponentHeader [component]="field.filterComponent" [group]="group" [field]="filterField" >
             </ng-container>
             <span style="padding-top: 10px;">
                 <button *ngIf="field.filterClearable && group.get(field.name).value" mat-button mat-icon-button aria-label="Clear" type="button"
@@ -19,13 +19,19 @@ import { IKlesHeaderFieldConfig } from '../../models/header-field.config.model';
     `,
     styles: [`mat-form-field {width: calc(100%)} 
         mat-icon {font-size: 16px; height:16px; width:16px} 
-        .mat-mdc-icon-button {padding-bottom: 4px; min-width: 0; width: 22px; height: 22px; flex-shrink: 0; line-height: 1; border-radius: 50%;}`
+        .mat-mdc-icon-button {padding-bottom: 4px; min-width: 0; width: 22px; height: 22px; flex-shrink: 0; line-height: 1; border-radius: 50%;}
+        .mat-sort-header-container {display: flex; justify-content: center;}`
     ]
 })
 export class KlesFormDynamicHeaderFilterComponent extends KlesFieldAbstract implements OnInit {
     field: IKlesHeaderFieldConfig;
+    filterField: IKlesHeaderFieldConfig;
+
     ngOnInit(): void {
         super.ngOnInit();
+
+        this.filterField = this.field;
+        delete this.filterField['label'];
     }
 
     stopPropagation(event) {
