3bb4799295bcfcf95b91dc9f9434b357058d5aa3
JCHAUT <j.chaut@3kles-consulting.com>
2024-07-09 15:24:42 +0200

    feat: add deleteoffset on treetable

--- projects/kles-material-table/src/lib/models/columnconfig.model.ts
+++ projects/kles-material-table/src/lib/models/columnconfig.model.ts
@@ -4,30 +4,31 @@ import { IKlesHeaderFieldConfig } from './header-field.config.model';
 import { AlignCell } from '../enums/align.enum';
 
 export interface KlesColumnConfig {
-    columnDef: string;
-    sticky?: boolean;
-    stickyEnd?: boolean;
-    visible: boolean;
-    disabled?: boolean;
-    name?: string;
-    ngClass?: any;
-    filterable?: boolean;
-    sortable?: boolean;
-    resizable?: boolean;
-    headerCell: IKlesHeaderFieldConfig;
-    cell: IKlesCellFieldConfig;
-    footerCell?: IKlesCellFieldConfig;
-    canUnfold?: boolean;
-    align?: AlignCell;
+  columnDef: string;
+  sticky?: boolean;
+  stickyEnd?: boolean;
+  visible: boolean;
+  disabled?: boolean;
+  name?: string;
+  ngClass?: any;
+  filterable?: boolean;
+  sortable?: boolean;
+  resizable?: boolean;
+  headerCell: IKlesHeaderFieldConfig;
+  cell: IKlesCellFieldConfig;
+  footerCell?: IKlesCellFieldConfig;
+  canUnfold?: boolean;
+  align?: AlignCell;
 }
 export interface KlesTreeColumnConfig extends KlesColumnConfig {
-    canExpand?: boolean;
-    paginator?: boolean;
-    paginatorOption?: {
-        pageSize?: number;
-        showFirstLastButtons?: boolean;
-        hidePageSize?: boolean;
-        pageSizeOptions?: number[]
-    }
+  canExpand?: boolean;
+  deleteOffset?: boolean;
+  paginator?: boolean;
+  paginatorOption?: {
+    pageSize?: number;
+    showFirstLastButtons?: boolean;
+    hidePageSize?: boolean;
+    pageSizeOptions?: number[]
+  }
 
 }
