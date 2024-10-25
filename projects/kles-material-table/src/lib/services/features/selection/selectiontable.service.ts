9be0e49bb8b727229f825d4916eed90e0d2d16bb
Charles Da Silva Costa <CDASILVA@3KLES.local>
2024-10-25 22:08:51 +0200

    fix: refactor and fix perf selection table service

--- projects/kles-material-table/src/lib/services/features/selection/selectiontable.service.ts
+++ projects/kles-material-table/src/lib/services/features/selection/selectiontable.service.ts
@@ -15,22 +15,26 @@ export class KlesSelectionTableService implements KlesTableBaseService {
     changed.removed.forEach((group: FormGroup) => {
       group.controls[this.columnSelect].patchValue(false, { emitEvent: false });
     });
-    this.table.columns().filter(f => f.columnDef === this.columnSelect)
-      .map(m => m.headerCell.indeterminate = !this.table.selection.isEmpty());
-    if (this.table.selection.isEmpty()) {
-      this.table.formHeader.controls[this.columnSelect]?.patchValue(false, { emitEvent: false });
-    }
-    this.table.ref.markForCheck();
+
+    changed.added.forEach((group: FormGroup) => {
+      group.controls[this.columnSelect].patchValue(true, { emitEvent: false });
+    });
+    this.table._onSelected.emit(this.table.selection.selected);
+
+    this.updateColumnSelect();
   }
+
 
   changeSelectionHeader(e: any) {
     if (e.column.columnDef === this.columnSelect) {
       const val = (e.group as UntypedFormGroup).controls[this.columnSelect].value;
-      this.table.getFormArray().controls
-        .filter((e: UntypedFormGroup) => e.controls[this.columnSelect]?.enabled)
-        .forEach((e: UntypedFormGroup) => {
-          e.controls[this.columnSelect]?.patchValue(val);
-        });
+      if (val) {
+        this.table.selection.select(...this.table.dataSource.filteredData);
+      } else {
+        this.table.selection.deselect(...this.table.dataSource.filteredData);
+      }
+    } else {
+      this.updateColumnSelect();
     }
   }
 
@@ -39,61 +43,42 @@ export class KlesSelectionTableService implements KlesTableBaseService {
       if (e.column.columnDef === this.columnSelect && e.row) {
         if (this.table.dataSource.filteredData.includes(e.group)) {
           if ((e.group as UntypedFormGroup).controls[e.column.columnDef].value) {
-            if (!this.table.selection.isMultipleSelection()) {
-              this.table.getFormArray().controls
-                .filter((row: UntypedFormGroup) => row.value._id !== e.group.value._id)
-                .forEach((row: UntypedFormGroup) => {
-                  row.controls[this.columnSelect]?.patchValue(false, { emitEvent: false });
-                });
-            }
-
             this.table.selection.select(e.group);
-
           } else {
             this.table.selection.deselect(e.group);
           }
-          this.table._onSelected.emit(this.table.selection.selected);
-        } else {
-          (e.group as UntypedFormGroup).controls[e.column.columnDef]?.patchValue(false, { onlySelf: true, emitEvent: false });
-          this.table.selection.deselect(e.row);
         }
       }
-      if (this.isAllSelected()) {
 
-        this.table.columns.update((columns) => {
-          columns.filter(f => f.columnDef === this.columnSelect).forEach(m => m.headerCell.indeterminate = false);
-          return [...columns];
-        });
-
-        this.table.formHeader.controls[this.columnSelect]?.patchValue(true, { onlySelf: true, emitEvent: false });
-        this.table.tableService.onSelectIndeterminate.next(false);
-
-      } else {
-        this.table.columns().filter(f => f.columnDef === this.columnSelect)
-          .map(m => m.headerCell.indeterminate = !this.table.selection.isEmpty());
-
-        this.table.columns.update((columns) => {
-          columns.filter(f => f.columnDef === this.columnSelect)
-            .forEach(m => m.headerCell.indeterminate = !this.table.selection.isEmpty());
-          return [...columns];
-        });
-
-        this.table.tableService.onSelectIndeterminate.next(!this.table.selection.isEmpty());
-        if (this.table.selection.isEmpty()) {
-          this.table.formHeader.controls[this.columnSelect]?.patchValue(false, { onlySelf: true, emitEvent: false });
-        }
-      }
-      this.table.ref.markForCheck();
     }
   }
 
-  isAllSelected(): boolean {
-    if (this.table) {
-      const numSelected = this.table.selection.selected
-        .filter(s => this.table.dataSource.filteredData.includes(s)).length;
-      const numRows = this.table.dataSource.filteredData.length;
-      return numSelected === numRows;
+  protected updateColumnSelect() {
+    let indeterminate = false;
+    let selectAll = false;
+
+    if (this.table?.selection.isEmpty()) {
+      indeterminate = false;
+      selectAll = false;
+    } else {
+      if (this.table.dataSource.filteredData.every((record) => this.table.selection.isSelected(record))) {
+        indeterminate = false;
+        selectAll = true;
+
+      } else {
+        selectAll = false;
+        indeterminate = this.table.selection.hasValue();
+      }
+
     }
-    return false;
+    this.table?.columns.update((columns) => {
+      columns.filter(f => f.columnDef === this.columnSelect)
+        .forEach(m => m.headerCell.indeterminate = indeterminate);
+      return [...columns];
+    });
+    this.table?.formHeader.controls[this.columnSelect]?.patchValue(selectAll, { emitEvent: false });
+
+    this.table?.tableService.onSelectIndeterminate.next(indeterminate);
+    this.table.ref.markForCheck();
   }
 }
