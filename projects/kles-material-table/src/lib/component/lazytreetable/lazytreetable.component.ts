c8d48895fc266a65c7dc9e2f437d76ccb8fc7061
Charles Da Silva Costa <CDASILVA@3KLES.local>
2025-03-08 22:54:05 +0100

    fix: load after select all click

--- projects/kles-material-table/src/lib/component/lazytreetable/lazytreetable.component.ts
+++ projects/kles-material-table/src/lib/component/lazytreetable/lazytreetable.component.ts
@@ -49,6 +49,13 @@ export class KlesLazyTreetableComponent<T> extends KlesTreetableComponent<T> imp
 
   ngOnInit(): void {
     super.ngOnInit();
+    this.filteredValues$.next(
+      this.columns()
+        .filter(column => column.filterable)
+        .map(column => {
+          return { [column.columnDef]: this.formHeader.controls[column.columnDef].value };
+        })
+        .reduce((a, b) => ({ ...a, ...b }), {}));
   }
 
   ngOnChanges(changes: SimpleChanges): void {
